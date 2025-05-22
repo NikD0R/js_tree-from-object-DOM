680de9f482fe8159a1ed7675216592cb80f89131
Nikita Doroshenko <nikitokd05@gmail.com>
2025-05-22 13:39:28 +0300

    make build

---
+++ js_tree-from-object-DOM.f75de5e1.js
@@ -0,0 +1,31 @@
+'use strict';
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector('#tree');
+function createTree(element, data) {
+    const ul = document.createElement('ul');
+    for (const key of Object.keys(data)){
+        const li = document.createElement('li');
+        li.textContent = key;
+        if (Object.keys(data).length !== 0) createTree(li, data[key]);
+        ul.append(li);
+    }
+    element.append(ul);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=js_tree-from-object-DOM.f75de5e1.js.map
